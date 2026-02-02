3e4e58af6b7602dca0809b8f1368d985c577af09
John Bridgwater <jbridgwater@goodyear.com>
2026-02-02 17:03:55 +0000

    Improvements to signing scripts

--- CreateSignedScript.ps1
+++ CreateSignedScript.ps1
@@ -6,9 +6,6 @@ param(
 # git clone https://github.com/goodyear/grips-signtool.git
 # See GRIPS OneNote for more information
 
-#$cert = Get-ChildItem -Path Cert:\CurrentUser\My | Where-Object {$_.Subject -like "*GRIPS_Codesign.goodyear.com*"}
-#Set-AuthenticodeSignature -FilePath $Path -Certificate $cert
-
 $signToolPath = "$PSScriptRoot\..\grips-signtool\AzureArtifactSigning\microsoft.windows.sdk.buildtools\10.0.26100.7463\bin\10.0.26100.0\x64\signtool.exe"
 $DlibDllPath = "$PSScriptRoot\..\grips-signtool\AzureArtifactSigning\microsoft.artifactsigning.client\1.0.115\bin\x64\Azure.CodeSigning.Dlib.dll"
 $metaDataFilePath = "$PSScriptRoot\..\grips-signtool\AzureArtifactSigning\GRIPS-codesign.json"
